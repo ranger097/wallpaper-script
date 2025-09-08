0b3a12ada04ee9d954f1d2c23d23a2ebf64f50b3
ranger097 <mrgentry13@gmail.com>
2025-09-07 21:58:58 -0400

    added more wallpapers

--- main.py
+++ main.py
@@ -22,17 +22,27 @@ def choosing_wallpaper(stdscr):
     if pressed_enter == ord("\n"):
         stdscr.clear()
         stdscr.addstr(0,0,"Please choose a wallpaper :")
-        stdscr.addstr(2,0,"1. Solo Leveling")
-        stdscr.addstr(4,0,"2. Demon Slayer")
-        stdscr.addstr(6,0,"3. Naruto")
+        stdscr.addstr(1,0,"(a) . Dan Da Dan 1")
+        stdscr.addstr(2,0,"(b) . Demon Slayer 1")
+        stdscr.addstr(3,0,"(c) . Jujustu Kaisen 1")
+        stdscr.addstr(4,0,"(d) . Jujustu Kaisen 2")
+        stdscr.addstr(5,0,"(e) . Jujustu Kaisen 3")
+        stdscr.addstr(6,0,"(f) . Kaiju 1")
+        stdscr.addstr(7,0,"(g) . Kaimen Riders 1")
+        stdscr.addstr(8,0,"(h) . Kill La Kill 1")
+        stdscr.addstr(9,0,"(i) . Naruto 1")
+        stdscr.addstr(10,0,"(j) . Pokemon 1")
+        stdscr.addstr(11,0,"(k) . Pokemon 2")
+        stdscr.addstr(12,0,"(l) . Pokemon 3")
+        stdscr.addstr(13,0,"(m) . Solo Leveling 1")
         stdscr.refresh()
         pick_wallpaper = stdscr.getch()
-        if pick_wallpaper == ord("1"):
+        if pick_wallpaper == ord("a"):
             try: 
                 subprocess.run(["swww" ,  "kill"], check=False)
                 subprocess.Popen(["swww-daemon"])
-                subprocess.run(["swww", "img", "/home/ranger/wallpaper-script/solo-leveling.png"], check=True)
-                subprocess.run(["wal", "-i", "/home/ranger/wallpaper-script/ds.jpg"],check=True)
+                subprocess.run(["swww", "img", "/home/ranger/wallpaper-script/Wallpapers/Dan_Da_Dan_1.png"], check=True)
+                subprocess.run(["wal", "-i", "/home/ranger/wallpaper-script/Wallpapers/Dan_Da_Dan_1.png"],check=True)
                 subprocess.run(["hyprctl", "reload"],check=True)
             except:
                 stdscr.clear()
@@ -40,23 +50,188 @@ def choosing_wallpaper(stdscr):
                 stdscr.addstr(2,0,"check the path to your image.")
                 stdscr.refresh()
                 time.sleep(5)
-                choosing_wallpaper()
+                choosing_wallpaper(stdscr)
                 
-        elif pick_wallpaper == ord("2"):
+        elif pick_wallpaper == ord("b"):
             try: 
                 subprocess.run(["swww" ,  "kill"], check=False)
                 subprocess.Popen(["swww-daemon"])
-                subprocess.run(["swww", "img", "/home/ranger/wallpaper-script/demonslayer.jpg"], check=True)
-                subprocess.run(["wal", "-i", "/home/ranger/wallpaper-script/demonslayer.jpg"],check=True)
+                subprocess.run(["swww", "img", "/home/ranger/wallpaper-script/Wallpapers/Demon_Slayer_1.jpg"], check=True)
+                subprocess.run(["wal", "-i", "/home/ranger/wallpaper-script/Wallpapers/Demon_Slayer_1.jpg"],check=True)
                 subprocess.run(["hyprctl", "reload"],check=True)
             except:
+                stdscr.clear()
+                stdscr.addstr(0, 0,"Sorry this wallpaper is unavailable ,")
+                stdscr.addstr(2,0,"check the path to your image.")
+                stdscr.refresh()
+                time.sleep(5)
+                choosing_wallpaper(stdscr)
+
+        elif pick_wallpaper == ord("c"):
+            try: 
                 subprocess.run(["swww" ,  "kill"], check=False)
                 subprocess.Popen(["swww-daemon"])
-                subprocess.run(["swww", "img", "/home/ranger/wallpaper-script/demonslayer.jpg"], check=True)
-                subprocess.run(["wal" , "-i" , "/home/ranger/wallpaper-script/demonslayer.jpg"],check=True)
+                subprocess.run(["swww", "img", "/home/ranger/wallpaper-script/Wallpapers/JJK_1.jpeg"], check=True)
+                subprocess.run(["wal", "-i", "/home/ranger/wallpaper-script/Wallpapers/JJK_1.jpeg"],check=True)
                 subprocess.run(["hyprctl", "reload"],check=True)
+            except:
+               stdscr.clear()
+               stdscr.addstr(0, 0,"Sorry this wallpaper is unavailable ,")
+               stdscr.addstr(2,0,"check the path to your image.")
+               stdscr.refresh()
+               time.sleep(5)
+               choosing_wallpaper(stdscr)
+
+        elif pick_wallpaper == ord("d"):
+            try: 
+                subprocess.run(["swww" ,  "kill"], check=False)
+                subprocess.Popen(["swww-daemon"])
+                subprocess.run(["swww", "img", "/home/ranger/wallpaper-script/Wallpapers/JJK_2.jpeg"], check=True)
+                subprocess.run(["wal", "-i", "/home/ranger/wallpaper-script/Wallpapers/JJK_2.jpeg"],check=True)
+                subprocess.run(["hyprctl", "reload"],check=True)
+            except:
+                stdscr.clear()
+                stdscr.addstr(0, 0,"Sorry this wallpaper is unavailable ,")
+                stdscr.addstr(2,0,"check the path to your image.")
+                stdscr.refresh()
+                time.sleep(5)
+                choosing_wallpaper(stdscr)
+
+        elif pick_wallpaper == ord("e"):
+            try: 
+                subprocess.run(["swww" ,  "kill"], check=False)
+                subprocess.Popen(["swww-daemon"])
+                subprocess.run(["swww", "img", "/home/ranger/wallpaper-script/Wallpapers/JJK_3.jpeg"], check=True)
+                subprocess.run(["wal", "-i", "/home/ranger/wallpaper-script/Wallpapers/JJK_3.jpeg"],check=True)
+                subprocess.run(["hyprctl", "reload"],check=True)
+            except:
+                stdscr.clear()
+                stdscr.addstr(0, 0,"Sorry this wallpaper is unavailable ,")
+                stdscr.addstr(2,0,"check the path to your image.")
+                stdscr.refresh()
+                time.sleep(5)
+                choosing_wallpaper(stdscr)
+
+        elif pick_wallpaper == ord("f"):
+            try: 
+                subprocess.run(["swww" ,  "kill"], check=False)
+                subprocess.Popen(["swww-daemon"])
+                subprocess.run(["swww", "img", "/home/ranger/wallpaper-script/Wallpapers/Kaiju_1.jpg"], check=True)
+                subprocess.run(["wal", "-i", "/home/ranger/wallpaper-script/Wallpapers/Kaiju_1.jpg"],check=True)
+                subprocess.run(["hyprctl", "reload"],check=True)
+            except:
+                stdscr.clear()
+                stdscr.addstr(0, 0,"Sorry this wallpaper is unavailable ,")
+                stdscr.addstr(2,0,"check the path to your image.")
+                stdscr.refresh()
+                time.sleep(5)
+                choosing_wallpaper(stdscr)
+
+        elif pick_wallpaper == ord("g"):
+            try: 
+                subprocess.run(["swww" ,  "kill"], check=False)
+                subprocess.Popen(["swww-daemon"])
+                subprocess.run(["swww", "img", "/home/ranger/wallpaper-script/Wallpapers/Kaimen_riders_1.png"], check=True)
+                subprocess.run(["wal", "-i", "/home/ranger/wallpaper-script/Wallpapers/Kaimen_riders_1.png"],check=True)
+                subprocess.run(["hyprctl", "reload"],check=True)
+            except:
+                stdscr.clear()
+                stdscr.addstr(0, 0,"Sorry this wallpaper is unavailable ,")
+                stdscr.addstr(2,0,"check the path to your image.")
+                stdscr.refresh()
+                time.sleep(5)
+                choosing_wallpaper(stdscr)
+
+        elif pick_wallpaper ==ord("h"):
+            try: 
+                subprocess.run(["swww" ,  "kill"], check=False)
+                subprocess.Popen(["swww-daemon"])
+                subprocess.run(["swww", "img", "/home/ranger/wallpaper-script/Wallpapers/Kill_La_Kill_1.webp"], check=True)
+                subprocess.run(["wal", "-i", "/home/ranger/wallpaper-script/Wallpapers/Kill_La_Kill_1.webp"],check=True)
+                subprocess.run(["hyprctl", "reload"],check=True)
+            except:
+                stdscr.clear()
+                stdscr.addstr(0, 0,"Sorry this wallpaper is unavailable ,")
+                stdscr.addstr(2,0,"check the path to your image.")
+                stdscr.refresh()
+                time.sleep(5)
+                choosing_wallpaper(stdscr)
+
+        elif pick_wallpaper == ord("i"):
+            try: 
+                subprocess.run(["swww" ,  "kill"], check=False)
+                subprocess.Popen(["swww-daemon"])
+                subprocess.run(["swww", "img", "/home/ranger/wallpaper-script/Wallpapers/Naruto_1.jpeg"], check=True)
+                subprocess.run(["wal", "-i", "/home/ranger/wallpaper-script/Wallpapers/Naruto_1.jpeg"],check=True)
+                subprocess.run(["hyprctl", "reload"],check=True)
+            except:
+               stdscr.clear()
+               stdscr.addstr(0, 0,"Sorry this wallpaper is unavailable ,")
+               stdscr.addstr(2,0,"check the path to your image.")
+               stdscr.refresh()
+               time.sleep(5)
+               choosing_wallpaper(stdscr)
+               
+        elif pick_wallpaper == ord("j"):
+            try: 
+                subprocess.run(["swww" ,  "kill"], check=False)
+                subprocess.Popen(["swww-daemon"])
+                subprocess.run(["swww", "img", "/home/ranger/wallpaper-script/Wallpapers/Pokemon_1.jpg"], check=True)
+                subprocess.run(["wal", "-i", "/home/ranger/wallpaper-script/Wallpapers/Pokemon_1.jpg"],check=True)
+                subprocess.run(["hyprctl", "reload"],check=True)
+            except:
+                stdscr.clear()
+                stdscr.addstr(0, 0,"Sorry this wallpaper is unavailable ,")
+                stdscr.addstr(2,0,"check the path to your image.")
+                stdscr.refresh()
+                time.sleep(5)
+                choosing_wallpaper(stdscr)
+
+        elif pick_wallpaper == ord("k"):
+            try: 
+                subprocess.run(["swww" ,  "kill"], check=False)
+                subprocess.Popen(["swww-daemon"])
+                subprocess.run(["swww", "img", "/home/ranger/wallpaper-script/Wallpapers/Pokemon_2.jpg"], check=True)
+                subprocess.run(["wal", "-i", "/home/ranger/wallpaper-script/Wallpapers/Pokemon_2.jpg"],check=True)
+                subprocess.run(["hyprctl", "reload"],check=True)
+            except:
+                stdscr.clear()
+                stdscr.addstr(0, 0,"Sorry this wallpaper is unavailable ,")
+                stdscr.addstr(2,0,"check the path to your image.")
+                stdscr.refresh()
+                time.sleep(5)
+                choosing_wallpaper(stdscr)
+        elif pick_wallpaper == ord("l"):
+            try: 
+                subprocess.run(["swww" ,  "kill"], check=False)
+                subprocess.Popen(["swww-daemon"])
+                subprocess.run(["swww", "img", "/home/ranger/wallpaper-script/Wallpapers/Pokemon_3.jpg"], check=True)
+                subprocess.run(["wal", "-i", "/home/ranger/wallpaper-script/Wallpapers/Pokemon_3.jpg"],check=True)
+                subprocess.run(["hyprctl", "reload"],check=True)
+            except:
+                stdscr.clear()
+                stdscr.addstr(0, 0,"Sorry this wallpaper is unavailable ,")
+                stdscr.addstr(2,0,"check the path to your image.")
+                stdscr.refresh()
+                time.sleep(5)
+                choosing_wallpaper(stdscr)
+        elif pick_wallpaper == ord("m"):
+            try: 
+                subprocess.run(["swww" ,  "kill"], check=False)
+                subprocess.Popen(["swww-daemon"])
+                subprocess.run(["swww", "img", "/home/ranger/wallpaper-script/Wallpapers/Solo_leveling_1.png"], check=True)
+                subprocess.run(["wal", "-i", "/home/ranger/wallpaper-script/Wallpapers/Solo_leveling_1.png"],check=True)
+                subprocess.run(["hyprctl", "reload"],check=True)
+            except:
+                stdscr.clear()
+                stdscr.addstr(0, 0,"Sorry this wallpaper is unavailable ,")
+                stdscr.addstr(2,0,"check the path to your image.")
+                stdscr.refresh()
+                time.sleep(5)
+                choosing_wallpaper(stdscr)
+       
         else:
-            choosing_wallpaper()
+            choosing_wallpaper(stdscr)
 
 
 
